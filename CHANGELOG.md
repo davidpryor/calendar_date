5d1da50b2b5fb474cb0b5e3d05beb9f5fb37cd13
David Pryor <david@macrofactorapp.com>
2025-08-04 16:00:49 -0400

    chore: add melos, add fvm, temporarily remove package code

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-## 0.9.0
-
-* Adds base CalendarDate and CalendarDateTime classes
-* Adds Stride class for creating generator iterators for iterating between two CalendarDate/CalendarDateTime
-* Basic tests for Stride
--- lib/calendar_date.dart
+++
@@ -1,8 +0,0 @@
-export 'src/calendar_date.dart'
-    show CalendarDate, CalendarDateConversion, CalendarDateStrideUtil;
-export 'src/calendar_datetime.dart'
-    show
-        CalendarDateTime,
-        CalendarDateTimeConversion,
-        CalendarDateTimeStrideUtil;
-export 'src/stride.dart' show Stridable, Stride;
