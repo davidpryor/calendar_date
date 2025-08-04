3affdd4c0e294839bb223e71c4788324f6e9b1a6
David Pryor <david@macrofactorapp.com>
2025-08-04 12:14:07 -0400

    chore: expand exports

--- lib/calendar_date.dart
+++ lib/calendar_date.dart
@@ -1,3 +1,8 @@
-export 'src/calendar_date.dart' show CalendarDate;
-export 'src/calendar_datetime.dart' show CalendarDateTime;
+export 'src/calendar_date.dart'
+    show CalendarDate, CalendarDateConversion, CalendarDateStrideUtil;
+export 'src/calendar_datetime.dart'
+    show
+        CalendarDateTime,
+        CalendarDateTimeConversion,
+        CalendarDateTimeStrideUtil;
 export 'src/stride.dart' show Stridable, Stride;
